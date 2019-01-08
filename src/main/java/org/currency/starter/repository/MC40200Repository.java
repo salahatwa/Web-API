a952d974369f8f664765fd08c7b2dc7890dd399f
ssatwa <ssatwa@WSRUHHQ1995.ME.Alinma.internal>
2019-01-08 11:37:40 +0300

    Update Currency Repository

--- src/main/java/org/currency/starter/repository/MC40200Repository.java
+++ src/main/java/org/currency/starter/repository/MC40200Repository.java
@@ -3,6 +3,6 @@ package org.currency.starter.repository;
 import org.currency.starter.domain.MC40200;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MC40200Repository extends JpaRepository<MC40200, String> {
+public interface MC40200Repository extends JpaRepository<MC40200, Integer> {
 
 }
